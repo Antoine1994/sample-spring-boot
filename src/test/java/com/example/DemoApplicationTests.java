d71e0d3622882a765ff690f9e337e896e7df093d
bertin <a.bertin@esitpa.fr>
2019-09-12 12:04:45 +0200

    modification demaoapplicationtest

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -1,22 +1,95 @@
 package com.example;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-public class DemoApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertTrue;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+	@Test
+	public void contextLoads() {
 	}
 
-	public static Integer additionner(Integer a, Integer b) throws Exception {
-		if (a != null && b != null) {
-			return a+b;
+	@Test
+	public void myTest() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void calculate_methods_first_param_should_not_be_null() throws Exception {
+		try {
+			DemoApplication.additionner(null,2);
+			DemoApplication.soustraire(null,2);
+			DemoApplication.multiplier(null,2);
+			assertTrue(false);
 		}
-		throw new Exception();
+		catch (Exception e) {
+			assertTrue(true);
+		}
 	}
 
-	public static Integer soustraire(Integer a, Integer b) throws Exception {
-		if (a != null && b != null) {
-			return a-b;
+	@Test
+	public void calculate_methods_second_param_should_not_be_null() throws Exception {
+		try {
+			DemoApplication.additionner(1,null);
+			DemoApplication.soustraire(1,null);
+			DemoApplication.multiplier(1,null);
+			assertTrue(false);
+		}
+		catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+
+	@Test
+	public void additionner_should_give_a_number_with_two_integers() throws Exception {
+		//GIVEN
+
+		//WHEN
+		Integer result = DemoApplication.additionner(1,2);
+		//THEN
+		Integer expected = 3;
+		if (result == expected) {
+			assertTrue(true);
+		}
+		else {
+			assertTrue(false);
+		}
+	}
+
+	@Test
+	public void soustraire_should_give_a_number_with_two_integers() throws Exception {
+		//GIVEN
+
+		//WHEN
+		Integer result = DemoApplication.soustraire(1,2);
+		//THEN
+		Integer expected = -1;
+		if (result == expected) {
+			assertTrue(true);
+		}
+		else {
+			assertTrue(false);
+		}
+	}
+
+	@Test
+	@Ignore
+	public void multiplier_should_give_a_number_with_two_integers() throws Exception {
+		//GIVEN
+
+		//WHEN
+		Integer result = DemoApplication.multiplier(1,2);
+		//THEN
+		Integer expected = 2;
+		if (result == expected) {
+			assertTrue(true);
+		}
+		else {
+			assertTrue(false);
 		}
 
 		// Test de code dupliqué
@@ -41,38 +114,6 @@ public class DemoApplication {
 		System.out.println("i:"+i);
 		i = 10;
 		System.out.println("i:"+i);
-
-		throw new Exception();
 	}
 
-	public static Integer multiplier(Integer a, Integer b) throws Exception {
-		if (a != null && b != null) {
-			return a*b;
-		}
-
-		// Test de code dupliqué
-		Integer i = 0;
-		i = 1;
-		System.out.println("i:"+i);
-		i = 2;
-		System.out.println("i:"+i);
-		i = 3;
-		System.out.println("i:"+i);
-		i = 4;
-		System.out.println("i:"+i);
-		i = 5;
-		System.out.println("i:"+i);
-		i = 6;
-		System.out.println("i:"+i);
-		i = 7;
-		System.out.println("i:"+i);
-		i = 8;
-		System.out.println("i:"+i);
-		i = 9;
-		System.out.println("i:"+i);
-		i = 10;
-		System.out.println("i:"+i);
-
-		throw new Exception();
-	}
 }
